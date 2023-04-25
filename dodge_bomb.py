baa2355782c3f225da23e1fa5d72cc8714fc6558
yukihisa <c0b22206093@edu.teu.ac.jp>
2023-04-25 15:47:44 +0900

    演習５

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -10,6 +10,22 @@ delt = {pg.K_UP: (0,-1),
         pg.K_RIGHT: (+1,0)
         }
 
+
+def check_bound(scr_rct: pg.Rect,obj_rct:pg.Rect):
+    """
+    オブジェクトが画面内か画面外かを判定する関数
+    引数1:画面SurfaceのRect
+    引数2:こうかとん、または爆弾SurfaceのRect
+    戻り値:横方向、縦方向のはみ出し判定結果（画面内:Ture/画面外:False）
+    """
+    yoko,tate = True,True
+    if obj_rct.left < scr_rct.left or scr_rct.right < obj_rct.right:
+        yoko = False
+    if obj_rct.top < scr_rct.top or scr_rct.bottom < obj_rct.bottom:
+        tate = False
+    return yoko, tate
+
+
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
     screen = pg.display.set_mode((1600, 900))
@@ -17,17 +33,23 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+
     tmr = 0
+
     bb_img = pg.Surface((20,20))
     pg.draw.circle(bb_img,(255,0,0),(10,10),10)
     bb_img.set_colorkey((0,0,0))
+
     bb_x = random.randint(0,1600)
     bb_y = random.randint(0,900)
+
     vx,vy = +1,+1
+
     bb_rct = bb_img.get_rect()
     bb_rct.center = bb_y,bb_x
+
     kk_rct = kk_img.get_rect()
-    kk_rct.center = 400,900
+    kk_rct.center = 900,400
 
     while True:
         for event in pg.event.get():
@@ -41,9 +63,20 @@ def main():
             if key_lst[k]:
                 kk_rct.move_ip(mv)
 
+        if check_bound(screen.get_rect(), kk_rct) != (True, True):
+            for k, mv in delt.items():
+                if key_lst[k]:
+                    kk_rct.move_ip(-mv[0], -mv[1]) 
+
+
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, kk_rct)
         bb_rct.move_ip(vx,vy)
+        yoko, tate = check_bound(screen.get_rect(), bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img,bb_rct)
 
         pg.display.update()
